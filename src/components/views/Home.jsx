f4a74df219ad0f45fc24ce2ee2f95c1e50a2df31
Canute <93085939+BetaCan@users.noreply.github.com>
2024-08-15 22:33:45 +0100

    start now

--- src/components/views/Home.jsx
+++ src/components/views/Home.jsx
@@ -1,15 +1,15 @@
-import { useAuth } from "../auth/useAuth";
-import "./Home.scss";
+import { useAuth } from '../auth/useAuth';
+import './Home.scss';
 
 function Home() {
   const { loggedInUser } = useAuth();
-  const message = loggedInUser
-    ? `${loggedInUser.name}!`
-    : "Welcome! Please log In";
+  const message = loggedInUser ? `${loggedInUser.name}!` : 'Welcome! Please log In';
 
   return (
     <>
-      <h1>Welcome </h1><h2>{message}</h2>
+      <h1>Welcome </h1>
+      <h2>{message}</h2>
+      <p> THIS IS THE STYLYING WRIRD THINGY </p>
     </>
   );
 }
