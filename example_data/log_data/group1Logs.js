d7d91c055c0aeaf5436ee35a985e58777eecc0c4
Canute <93085939+BetaCan@users.noreply.github.com>
2024-08-15 22:30:56 +0100

    hahaha

---
+++ example_data/log_data/group1Logs.js
@@ -0,0 +1,38 @@
+const group1Logs = [
+  {
+    LogID: 1,
+    LogName: "Week 1",
+    LogGroupID: 1,
+    LogSubmissiondate: "2023-08-13T00:10:00.000Z",
+    LogGroupName: "Group A01",
+  },
+  {
+    LogID: 2,
+    LogName: "Week 2",
+    LogGroupID: 1,
+    LogSubmissiondate: "2023-08-13T00:10:10.000Z",
+    LogGroupName: "Group A01",
+  },
+  {
+    LogID: 3,
+    LogName: "Week 3",
+    LogGroupID: 1,
+    LogSubmissiondate: "2023-08-13T00:09:00.000Z",
+    LogGroupName: "Group A01",
+  },
+  {
+    LogID: 4,
+    LogName: "Week 4",
+    LogGroupID: 1,
+    LogSubmissiondate: "2023-08-20T22:17:20.000Z",
+    LogGroupName: "Group A01",
+  },
+  {
+    LogID: 5,
+    LogName: "Week 5",
+    LogGroupID: 1,
+    LogSubmissiondate: "2023-08-27T22:12:23.000Z",
+    LogGroupName: "Group A01",
+  },
+];
+export default group1Logs;
